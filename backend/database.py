f47cf7965e1030c28f6cd44420cc53a3486d0123
GreenRewards <nguyenquochoangson@icloud.com>
2026-02-05 13:26:04 +0700

    Update all

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,22 @@
-from pymongo import MongoClient
-from pymongo import gridfs 
+from pymongo import MongoClient  # ĐÃ SỬA: MongoDBClient → MongoClient
+import gridfs
 import os
 
 MONGO_URI = "mongodb+srv://locnguyen2512cn_db_user:GreenRewards123@greenrewards.lqkojxf.mongodb.net/green_rewards?retryWrites=true&w=majority"
 
+# Sửa thành MongoClient
 client = MongoClient(MONGO_URI)
 
 db = client["green_rewards"]
-users = db["users"]
 
 # Collections
-users = db['users']
-vouchers = db['vouchers']
-user_vouchers = db['user_vouchers']
-partners = db['partners']  # Thêm mới
+users = db["users"]
+vouchers = db["vouchers"]
+user_vouchers = db["user_vouchers"]
+partners = db["partners"]
 
-# GridFS cho lưu trữ ảnh
-fs = gridfs.GridFS(db, collection='images')
+# GridFS cho lưu trữ ảnh - sửa thành gridfs.GridFS()
+fs = gridfs.GridFS(db, collection="images") 
 
 # Tạo index để tối ưu truy vấn
 partners.create_index([("name", 1)], unique=True)
